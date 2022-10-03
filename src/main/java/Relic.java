da675303c45c8bb2722925e3af1e6275cf0fa004
Arian Jafari <arian91@hotmail.com>
2022-10-03 15:58:09 +0200

    Adding relics database file

---
+++ src/main/java/Relic.java
@@ -0,0 +1,20 @@
+public class Relic extends GameObject
+{
+	private String description;
+
+	public Relic(String name, String rarity, String description)
+	{
+		super(name, rarity);
+		this.description = description;
+	}
+
+	public String toString()
+	{
+		StringBuilder printString = new StringBuilder();
+		printString.append(name + '\n');
+		printString.append(rarity + '\n');
+		printString.append(description + '\n');
+
+		return printString.toString();
+	}
+}
